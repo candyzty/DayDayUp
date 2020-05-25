86bd12d78d7afed7634f38a758d799306cfedd92
e <redgra0315@163.com>
2020-05-25 23:17:40 +0800

    day01 变量学习

--- days_01/变量/main.go
+++ days_01/变量/main.go
@@ -1,11 +1,31 @@
 package main
 
+// package 定义main 包
 import "fmt"
 
+// 全局的变量
+var a int    // int 类型默认值为：0，
+var b string // string 类型默认值为：空值.
+var c, d int = 1, 2
+var e, f = 1, "go"
+
+// 定义多个变量
+var (
+	l = 25
+	m = "Becca"
+	n = "3.54"
+)
+
+// main 函数只能有一个
 func main() {
-	// 推导类型
-	name := "redgra"
+	// 推导类型，只能在函数中定义
+	name := "Tom"
 	age := 21
 	set := 4.5423
 	fmt.Println(name, age, set)
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c, d)
+	fmt.Println(e, f, true)
+	fmt.Printf("l = %d m = %s n = %v", l, m, n)
 }
---
+++ days_01/变量/readme.md
@@ -0,0 +1,11 @@
+day01 
+学习go 语言的基础语法变量及基本使用
+
+#运行程序
+`go  run main.go `
+  
+#编程程序
+`go build  main.go `
+
+#安装自身包和依赖包
+`go install main.go`
