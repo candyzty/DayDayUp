27f2c7453b11c4c1633fd5c72377febbcb8f921c
redgra <redgra0315@163.com>
2020-06-05 18:14:57 +0800

    练习基本语法

---
+++ JUN/day_03/project/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+var parmInit = make(map[string]string)
+
+func forStatic() {
+	var (
+		y        string
+		userName string
+		userKey  string
+		number   int
+	)
+	fmt.Println("玩游戏的次数为:")
+	fmt.Scanln(&number)
+	for i := 1; i < number+1; i++ {
+		fmt.Printf("第%d次输入的值\n", i)
+		createUser(userName, userKey)
+	}
+	fmt.Println(parmInit)
+	fmt.Println("是否继续玩下去(y/n):")
+	fmt.Scanln(&y)
+	if y == "y" {
+		fmt.Println("继续进行:")
+		forStatic()
+	} else {
+		fmt.Println("退出！")
+		//os.Exit(0)
+	}
+
+}
+
+func createUser(value, key string) {
+	fmt.Println("第一个参数输入的是:value,第二个参数输入的是:key")
+	fmt.Scanln(&value, &key)
+	parmInit[value] = key
+	fmt.Println(parmInit)
+}
+
+func getMapInfo() {
+	getValue := ""
+	fmt.Println("查询已经存在的值！")
+	fmt.Scanln(&getValue)
+	name, ok := parmInit[getValue]
+	fmt.Println(name, ok)
+}
+
+func updateMapInfo() {
+	updateValue := ""
+	fmt.Println("更新map参数")
+	fmt.Println(updateValue)
+}
+func delectMapInfo() {
+	delValue := ""
+	fmt.Println("删除map值")
+	name, ok := parmInit[delValue]
+	fmt.Println(name, ok)
+	delete(parmInit, name)
+
+}
+
+//func buildParameters()  {
+//	m := map[string]string{
+//		"name":    "ccmouse",
+//		"course":  "golang",
+//		"site":    "imooc",
+//		"quality": "notbad",
+//	}
+//
+//	if  c == create {
+//		createUser()
+//	}else if g == get{
+//		getMapInfo()
+//	}else if u == update{
+//		updateMapInfo()
+//	}else if d == delect(){
+//		delectMapInfo()
+//	}else {
+//		fmt.Println("参数错误")
+//	}
+//
+//}
+func main() {
+	var x string
+	enter := "yes"
+	fmt.Println("请问是否开始答题(yes/no):")
+	fmt.Scanln(&x) //读取键盘的输入，通过操作地址，赋值给x和y   阻塞式
+	if x == enter {
+		fmt.Println("恭喜你,咱们开始了")
+		forStatic()
+		getMapInfo()
+	} else {
+		fmt.Println("游戏失败")
+		os.Exit(0)
+	}
+}
---
+++ JUN/day_03/project/pro01/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//c := 30
+	//switch c {
+	//case 20:
+	//	fmt.Println("c=20")
+	//case 10:
+	//	fmt.Println("a=10")
+	//case 15:
+	//	fmt.Println("c=15")
+	//default:
+	//	fmt.Println("c=30")
+	//}
+	fmt.Println(`
+	a)  aaa 
+    b)  sss 
+    d) ddd 
+	`)
+
+}
